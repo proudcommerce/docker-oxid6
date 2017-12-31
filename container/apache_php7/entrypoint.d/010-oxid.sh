5422c72a7924356e52ded80696912cfbe8c60d61
ProudCommerce <oxid@proudsourcing.de>
2017-12-31 16:41:20 +0100

    --dev by default

--- container/apache_php7/entrypoint.d/010-oxid.sh
+++ container/apache_php7/entrypoint.d/010-oxid.sh
@@ -4,7 +4,7 @@ set -e
 # bootstrap oxid files/project
 if [ ! -f /var/www/html/source/config.inc.php ]
 then
-	composer create-project --dev --keep-vcs oxid-esales/oxideshop-project . ${OXID_VERSION}
+	composer create-project --keep-vcs oxid-esales/oxideshop-project . ${OXID_VERSION}
 	mv /var/www/config.inc.php /var/www/html/source/config.inc.php
 	chown -R www-data:www-data /var/www/html/source/
 	echo "#####################################"
